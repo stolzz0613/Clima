fc4edb69e65ffd4acb14ee5d57d64ecbd8200442
stolzz0613 <andres0613@utp.edu.co>
2020-07-17 21:59:18 -0500

    Updates

---
+++ precache-manifest.d88390d7f5fc74ae7bb3b9ada50c9dd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3664c08a77da6760a3bdd9a82d77fd",
+    "url": "/Clima/index.html"
+  },
+  {
+    "revision": "53362953383b57b97737",
+    "url": "/Clima/static/css/main.324bf215.chunk.css"
+  },
+  {
+    "revision": "ece6a26b491f477efa18",
+    "url": "/Clima/static/js/2.5adef9fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clima/static/js/2.5adef9fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53362953383b57b97737",
+    "url": "/Clima/static/js/main.9fd0059f.chunk.js"
+  },
+  {
+    "revision": "f9a947c1b58a2fca82fc",
+    "url": "/Clima/static/js/runtime-main.947975a4.js"
+  }
+]);
